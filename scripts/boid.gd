0de399d4b86d4bda2360e8dfe662173cc9ce121c
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-03-13 00:10:16 -0400

    Draw and move boid

--- scripts/boid.gd
+++ scripts/boid.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 @export var min_speed := 200
-@export var max_speed := 800
+@export var max_speed := 400
 
 @export var protected_radius := 20
 @export var visible_radius := 64
@@ -11,6 +11,8 @@ extends CharacterBody2D
 @export var repulse_factor := 0.05
 @export var align_factor := 0.05
 @export var attract_factor := 0.005
+
+@export var color := Color.LIGHT_PINK
 
 
 # TODO: Replace with proper collision detection
@@ -21,10 +23,10 @@ var screen: Rect2
 #region Rules
 
 # TODO: Implement the following rules:
-# Turn: Avoid nearby obstacles
-# Repulse: Avoid overlapping with nearby boids
-# Align: Match velocity of nearby boids
-# Attract: Tend towards nearby boids
+# Avoid nearby walls or obstacles
+# Avoid overlapping with nearby boids
+# Match velocity of nearby boids
+# Tend towards nearby boids
 
 #endregion
 
@@ -42,7 +44,7 @@ func _physics_process(delta: float) -> void:
 
 	velocity += acceleration * delta
 	var speed := clampf(velocity.length(), min_speed, max_speed)
-	velocity = velocity.normalized() * speed
+	velocity = Vector2.from_angle(rotation) * speed
 	move_and_slide()
 
 
@@ -54,30 +56,11 @@ func draw_circline(from: Vector2, to: Vector2, radius: int, color: Color):
 
 
 func _draw() -> void:
-	var radius := ($CollisionShape.shape as CircleShape2D).radius
+	var radius := ($CollisionShape2D.shape as CircleShape2D).radius
 	var span := Vector2.RIGHT * radius * 0.75
-	$Sprite.draw_circline(span / -2, span / 2, radius)
+	draw_circline(span / -2, span / 2, radius, color)
 
 
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-#
-#
-#func _physics_process(delta: float) -> void:
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
+# IDEAS
+# Random wiggle to vary direction
+# Avoid threats- increase speed based on proximity/size
