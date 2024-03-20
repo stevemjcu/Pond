c598b37f43206927e37d3f9b0b2c269cc12078a2
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-03-19 23:36:17 -0400

    Reorder boid methods

--- scripts/boid.gd
+++ scripts/boid.gd
@@ -11,9 +11,11 @@ extends CharacterBody2D
 @export var match_factor := 2
 @export var approach_factor := 2
 
-
 var _color: Color
 var _group: String
+
+var _protected_neighbors: Array[Node2D]
+var _visible_neighbors: Array[Node2D]
 
 
 func assign(group: String, color: Color) -> void:
@@ -23,19 +25,34 @@ func assign(group: String, color: Color) -> void:
 	_color = color
 
 
-var protected_neighbors: Array[Node2D]:
-	get:
-		var bodies = $ProtectedArea.get_overlapping_bodies() as Array[Node2D]
-		return bodies.filter(func(b: Node2D): return b.is_in_group(_group))
+func _physics_process(delta: float) -> void:
+	_detect_neighbors()
+	var impulse := Vector2.ZERO
+	impulse += _avoid_screen_edge()
+	impulse += _avoid_neighbors()
+	impulse += _match_neighbors()
+	impulse += _approach_neighbors()
+	velocity += impulse * delta
+	rotation = velocity.normalized().angle()
+	_control_speed()
+	move_and_slide()
 
 
-var visible_neighbors: Array[Node2D]:
-	get:
-		var bodies := $VisibleArea.get_overlapping_bodies() as Array[Node2D]
-		return bodies.filter(func(b: Node2D): return b.is_in_group(_group))
+func _on_sprite_draw() -> void:
+	var shape := $CollisionShape2D.shape as CapsuleShape2D
+	$Sprite.draw_capsule(shape.height, shape.radius, _color)
 
 
-#region Rules
+func _detect_neighbors() -> void:
+	_protected_neighbors.clear()
+	for body: Node2D in $ProtectedArea.get_overlapping_bodies():
+		if body.is_in_group(_group):
+			_protected_neighbors.append(body)
+	_visible_neighbors.clear()
+	for body: Node2D in $VisibleArea.get_overlapping_bodies():
+		if body.is_in_group(_group):
+			_visible_neighbors.append(body)
+
 
 # Turn to avoid screen edge
 func _avoid_screen_edge() -> Vector2:
@@ -56,7 +73,7 @@ func _avoid_screen_edge() -> Vector2:
 # Tend to move away from nearby neighbors
 func _avoid_neighbors() -> Vector2:
 	var impulse := Vector2.ZERO
-	for neighbor in protected_neighbors:
+	for neighbor in _protected_neighbors:
 		impulse += position - neighbor.position
 	return impulse * avoid_factor
 
@@ -64,42 +81,23 @@ func _avoid_neighbors() -> Vector2:
 # Tend to align with visible neighbors
 func _match_neighbors() -> Vector2:
 	var impulse := Vector2.ZERO
-	if visible_neighbors.size() == 0:
+	if _visible_neighbors.size() == 0:
 		return impulse
-	for neighbor in visible_neighbors:
+	for neighbor in _visible_neighbors:
 		impulse += neighbor.velocity - velocity
-	return (impulse / visible_neighbors.size()) * match_factor
+	return (impulse / _visible_neighbors.size()) * match_factor
 
 
 # Tend to approach visible neighbors
 func _approach_neighbors() -> Vector2:
 	var impulse := Vector2.ZERO
-	if visible_neighbors.size() == 0:
+	if _visible_neighbors.size() == 0:
 		return impulse
-	for neighbor in visible_neighbors:
+	for neighbor in _visible_neighbors:
 		impulse += neighbor.position - position
-	return (impulse / visible_neighbors.size()) * approach_factor
-
-#endregion
+	return (impulse / _visible_neighbors.size()) * approach_factor
 
 
 func _control_speed() -> void:
 	var speed := clampf(velocity.length(), min_speed, max_speed)
 	velocity = velocity.normalized() * speed
-
-
-func _physics_process(delta: float) -> void:
-	var impulse := Vector2.ZERO
-	impulse += _avoid_screen_edge()
-	impulse += _avoid_neighbors()
-	impulse += _match_neighbors()
-	impulse += _approach_neighbors()
-	velocity += impulse * delta
-	rotation = velocity.normalized().angle()
-	_control_speed()
-	move_and_slide()
-
-
-func _on_sprite_draw() -> void:
-	var shape := $CollisionShape2D.shape as CapsuleShape2D
-	$Sprite.draw_capsule(shape.height, shape.radius, _color)
