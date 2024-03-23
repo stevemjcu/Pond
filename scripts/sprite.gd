af7651e85dba08aaf6c9a903755301d78cf3ae3e
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2024-03-22 22:18:41 -0400

    Update sprite.gd

--- scripts/sprite.gd
+++ scripts/sprite.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 
-func draw_circline(from: Vector2, to: Vector2, radius: int, color: Color):
+func draw_circline(from: Vector2, to: Vector2, radius: int, color: Color) -> void:
 	draw_circle(from, radius, color)
 	draw_circle(to, radius, color)
 	draw_line(from, to, color, radius * 2)
 
 
-func draw_capsule(height: int, radius: int, color: Color):
+func draw_capsule(height: int, radius: int, color: Color) -> void:
 	var span := Vector2.RIGHT * (height - radius * 2)
 	draw_circline(span / -2, span / 2, radius, color)
